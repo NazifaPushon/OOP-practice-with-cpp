44bb7d527dead0d08a88b2f2c460312c257df9f9
NazifaPushon <mashiat.pushon@gmail.com>
2022-03-12 23:35:54 +0600

    first opp

--- firstOOP.cpp
+++ firstOOP.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
+class Student {
+    public:
+    int id;
+    float gpa;
+};
 int main(){
+   
+    Student Alim;
+    Alim.id = 101;
+    Alim.gpa = 3.5;
+    cout << Alim.id << endl;
+    cout  << Alim.gpa << endl;
+
     return 0;
 }
